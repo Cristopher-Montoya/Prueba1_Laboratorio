b9afa687d780ef36a65fd34f1ca41c50d818934b
Cristopher-Montoya <124183887+Cristopher-Montoya@users.noreply.github.com>
2024-08-09 16:49:26 -0600

    Cristopher

---
+++ JuegoAhorcado.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prueba1_laboratorio;
+
+/**
+ *
+ * @author Jayma
+ */
+public interface JuegoAhorcado {
+    void inicializarPalabraSecreta();
+    void Jugar();
+}
